34d8862b48d90501c7e8328fd0c25dc5a01b8b44
nwni <nwnigm@gmail.com>
2018-01-08 18:56:32 -0600

    New Route, /red

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 //route['destino'] = ['fuente']
+$route['red'] = 'RedSemantica/red';
 $route['delete'] = 'pages/delete';
 $route['validacion'] = 'pages/view';
 $route['posts'] = 'pages/showPosts';
