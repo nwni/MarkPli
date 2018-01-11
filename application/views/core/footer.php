29354ace2f72c69538a12e1b3c899e0079927a0c
nwni <nwnigm@gmail.com>
2018-01-11 01:56:24 -0600

    Proper library for the dropdown

--- application/views/core/footer.php
+++ application/views/core/footer.php
@@ -1,6 +1,6 @@
 
-<script src="<?php echo base_url() ?>js/jquery.js"></script>
-<script src="<?php echo base_url() ?>js/bootstrap.js"></script>
+<script src="<?php echo base_url() ?>js/popper.js"></script>
+<script src="<?php echo base_url() ?>js/bootstrap4.js"></script>
 <!-- <script>
     $('.btn-registrar').on('click',  function(e){
         console.log("REGISTRADO");
