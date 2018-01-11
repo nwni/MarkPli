a18a35f536fc5d313091762f9c9ca2e8110d4c77
nwni <nwnigm@gmail.com>
2018-01-10 22:46:51 -0600

    Fields are now required. Removed 'Registrado' alert

--- application/views/core/footer.php
+++ application/views/core/footer.php
@@ -1,11 +1,11 @@
 
 <script src="<?php echo base_url() ?>js/jquery.js"></script>
 <script src="<?php echo base_url() ?>js/bootstrap.js"></script>
-<script>
+<!-- <script>
     $('.btn-registrar').on('click',  function(e){
         console.log("REGISTRADO");
         alert("Registrado!");
     });
-</script>
+</script> -->
 </body>
 </html>
--- application/views/registro2.php
+++ application/views/registro2.php
@@ -4,46 +4,37 @@
  	<br>
  	<center><h2>Registro de Usuarios</h2></center>
  	<br>
- 		<div class "row" >
+ 		<div class="row" >
  				<form method="POST"  action="<?php echo site_url('usuario/RegistroUsu');?>" role="form">
-					
-						
- 					 <div class="col-md-6">
-					
+ 					 <div class="col-md-12">
 						<label>Nombre de usuario:</label>
-						<input type="text"  name="NombreU" id="NombreU" required="" class="form-control" ">
-						
+						<input type="text"  name="NombreU" id="NombreU" class="form-control" required>
 						<div id='msg_usuario'></div>
 					</div>
-
-					<div class="col-md-6">
+					<div class="col-md-12">
 						<label >Contraseña:</label>
-							<input type="password"  name="ContraseñaU" id="ContraseñaU" required="" class="form-control">
-								</div>
-							 <div class="col-md-6">
-							 		<label >Tipo de usuario:</label>
-							 <select name="Nomtipo" class="form-control" required>
-                                              <option value="">Seleccione un tipo</option>  
-                                              <option value="Comunnity">Comunnity</option>  
-                                              <option value="Administrador">Administrador</option>
-                                              <option value="Uploader">Uploader</option>    
-                         </select>
-						<div id='msg_contraseña'></div>	
-
-							 </div>
-							 <br>
-						
+							<input type="password"  name="ContraseñaU" id="ContraseñaU" class="form-control" require>
+					</div>
+					<div class="col-md-12">
+						<label >Tipo de usuario:</label>
+							<select name="Nomtipo" class="form-control" required>
+								<option value="">Seleccione un tipo</option>  
+								<option value="Comunnity">Comunnity</option>  
+								<option value="Administrador">Administrador</option>
+								<option value="Uploader">Uploader</option>    
+                         	</select>
+						<div id='msg_contraseña'></div>
+					</div>
+				<br>	
 				<div class="row">
-					<div class="col-md-2">
-
-				 		<button class="btn btn-success btn-registrar" type="summit" name="btn-registrar" >Registrar</button>
-				 			 </div>
-				 		<div class="col-md-2">
-				 <a href="index.php/usuario/mostrar" class="btn btn-primary">Mostrar Usuarios Registrados</a>
-				 	 </div>
+					<div class="col-md-4">
+				 		<button type="submit" class="btn btn-success btn-registrar"  name="btn-registrar">Registrar</button>
+					</div>
+				 	<div class="col-md-4">
+				 		<a href="index.php/usuario/mostrar" class="btn btn-primary">Mostrar Usuarios Registrados</a>
+				 	</div>
 				  </div>
  			</form>
- 	
  	</div>
 </div>
 </body>
